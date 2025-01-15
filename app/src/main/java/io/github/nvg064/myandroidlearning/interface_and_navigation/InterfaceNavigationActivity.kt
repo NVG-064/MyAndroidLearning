78ad85f67c404f3ab57c1e392949d1c5673c8a83
NVG-064 <aryslow92@gmail.com>
2025-01-15 13:20:21 +0700

    Interface & Navigation: Rename to make it shorten

--- app/src/main/java/io/github/nvg064/myandroidlearning/interface_and_navigation/InterfaceNavigationActivity.kt
+++ app/src/main/java/io/github/nvg064/myandroidlearning/interface_and_navigation/InterfaceNavigationActivity.kt
@@ -6,17 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import io.github.nvg064.myandroidlearning.R
-import io.github.nvg064.myandroidlearning.databinding.ActivityInterfaceAndNavigationBinding
+import io.github.nvg064.myandroidlearning.databinding.ActivityInterfaceNavigationBinding
 
 class InterfaceNavigationActivity : AppCompatActivity() {
-    private lateinit var _binding: ActivityInterfaceAndNavigationBinding
+    private lateinit var _binding: ActivityInterfaceNavigationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
 
-        _binding = ActivityInterfaceAndNavigationBinding.inflate(layoutInflater)
+        _binding = ActivityInterfaceNavigationBinding.inflate(layoutInflater)
         setContentView(_binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
